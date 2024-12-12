229cfef75e265539026af068b8ca374cdb26ee6d
JO-GitHub23 <155160104+JO-GitHub23@users.noreply.github.com>
2024-12-12 09:28:25 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administ tasks."""
+    """Run joel tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'form.settings')
     try:
         from django.core.management import execute_from_command_line
